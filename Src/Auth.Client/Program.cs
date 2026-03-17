f2b9f1c4635268171f896707174121894445dd8d
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2026-03-17 18:00:52 -0400

    feat: iniciando implementação do mudblazor

--- Src/Auth.Client/Program.cs
+++ Src/Auth.Client/Program.cs
@@ -1,12 +1,27 @@
 using Auth.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+if(builder.HostEnvironment.IsDevelopment())
+{
+    builder.Services.AddScoped(sp => new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:7146")
+    });
+} else
+{
+    builder.Services.AddScoped(sp => new HttpClient 
+    { 
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+    });
+}
+
 
 await builder.Build().RunAsync();
