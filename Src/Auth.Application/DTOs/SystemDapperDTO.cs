55d4767ddabbf9c7f26c8615e67e23cb84f44e9e
Jovanne Sousa <jovanemaciel943@gmail.com>
2026-05-02 22:46:06 -0400

    corrigindo injeção de httpContext

--- Src/Auth.Application/DTOs/SystemDapperDTO.cs
+++ Src/Auth.Application/DTOs/SystemDapperDTO.cs
@@ -2,17 +2,9 @@
 {
     public class SystemDapperDTO
     {
-        public string SystemId { get; set; }
-        public string Name { get; set; }
-        public string Url { get; set; }
-        public List<ApplicationRoleDapperDTO> Permissoes { get; set; }
-
-        public SystemDapperDTO(List<ApplicationRoleDapperDTO> permissoes, string url, string name, string systemId)
-        {
-            Permissoes = permissoes;
-            Url = url;
-            Name = name;
-            SystemId = systemId;
-        }
+        public required string SystemId { get; set; }
+        public required string Name { get; set; }
+        public required string Url { get; set; }
+        public required List<ApplicationRoleDapperDTO> Permissoes { get; set; }
     }
 }
